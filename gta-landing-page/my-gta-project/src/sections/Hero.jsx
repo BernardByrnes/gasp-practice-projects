badfc5e18a096df6aac0847c78caa5ff70334abf
BernardByrnes <bernardtamboedu@gmail.com>
2025-11-21 20:41:10 +0300

    update

--- gta-landing-page/my-gta-project/src/sections/Hero.jsx
+++ gta-landing-page/my-gta-project/src/sections/Hero.jsx
@@ -1,4 +1,20 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import ComingSoon from "./ComingSoon";
+import { useMaskSettings } from "../../constants";
+
 const Hero = () => {
+  const { initialMaskPos, initialMaskSize, maskPos, maskSize } =
+    useMaskSettings();
+
+  useGSAP(() => {
+    gsap.set(".mask-wrapper", {
+      maskPosition: initialMaskPos,
+      maskSize: initialMaskSize,
+    });
+  });
+
   return (
     <section className="hero-section">
       <div className="size-full mask-wrapper">
@@ -8,7 +24,29 @@ const Hero = () => {
           alt="hero-logo"
           className="title-logo fade-out"
         />
+        <img
+          src="/images/watch-trailer.png"
+          alt="trailer"
+          className="trailer-logo fade-out"
+        />
+        <div className="play-img fade-out">
+          <img src="/images/play.png" alt="play" className="w-7 ml-1" />
+        </div>
       </div>
+
+      <div>
+        <img
+          src="/images/big-hero-text"
+          alt="logo"
+          className="size-full object-cover mask-logo"
+        />
+      </div>
+
+      <div className="fake-logo-wrapper">
+        <img src="/images/big-hero-text.svg" alt="" />
+      </div>
+
+      <ComingSoon />
     </section>
   );
 };
